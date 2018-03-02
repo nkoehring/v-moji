48dc5affa6ac2b4145178864d99f04dd0156097b
Norman Köhring <ext-norman.koehring@here.com>
2018-03-02 15:41:44 +0100

    component example

---
+++ example/child.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <p>I'm a child component :astonished: <slot></slot></p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'VueEasyMoji-TEST-APP',
+  data () {
+    return {
+    }
+  }
+}
+</script>
+
+<style>
+b { display: inline-block; width: 10em; }
+</style>
--- example/index.vue
+++ example/index.vue
@@ -7,16 +7,16 @@
     <div v-moji.ascii><b>ASCII Emojis:</b> Made with :heart: ;D and Vuejs</div>
     <div v-moji:unicode.ascii><b>Unicode output:</b> Made with :heart: ;D and Vuejs</div>
     <div v-moji:ascii><b>ASCII output:</b> Made with :heart: :wink: and Vuejs</div>
+    <div v-moji><child-component /></div>
+    <div v-moji><child-component>With Slot! :astonished:</child-component></div>
   </div>
 </template>
 
 <script>
+import ChildComponent from './child'
 export default {
   name: 'VueEasyMoji-TEST-APP',
-  data () {
-    return {
-    }
-  }
+  components: { ChildComponent }
 }
 </script>
 
